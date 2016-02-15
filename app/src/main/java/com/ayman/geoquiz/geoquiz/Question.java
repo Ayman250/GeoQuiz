cc60f8e4d83ee161344719074eba4fd9cc56bd35
Ayman250 <Aymanelmubark@gmail.com>
2016-02-14 22:09:10 -0600

    Initial commit

---
+++ app/src/main/java/com/ayman/geoquiz/geoquiz/Question.java
@@ -0,0 +1,31 @@
+package com.ayman.geoquiz.geoquiz;
+
+/**
+ * Created by Ayman on 2/14/2016.
+ */
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textREstID, boolean mAnswerTrue) {
+        mTextResId = textREstID;
+        mAnswerTrue = mAnswerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
